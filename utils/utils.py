7bfc0ff5da7a2ddd55f45560e601e1156add5e93
DL <21921009@zju.edu.cn>
2021-09-05 10:48:41 +0800

    Change the order of time_trainname

--- utils/utils.py
+++ utils/utils.py
@@ -4,4 +4,5 @@ class Utils:
     @staticmethod
     def get_train_name_with_time(name: str) -> str:
         timestr = time.strftime("%Y-%m-%d_%H:%M:%S", time.localtime())
-        return name + '_' + timestr
+        return timestr + '_' + name
+        
